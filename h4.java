a01b6788915a9e9ac71994274d020923bb37629c
Elakiyaaa <elakthamizh@gmail.com>
2017-07-20 10:23:57 +0530

    Add files via upload

---
+++ h4.java
@@ -0,0 +1,15 @@
+package GUVI;
+import java.util.Scanner;
+public class h4 {
+
+	public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+String string =s.next(); 
+char ch=string.charAt(0);
+boolean d=Character.isDigit(ch);
+	System.out.println(d);
+}
+		}
+
+	
+
